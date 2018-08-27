e2e82cf51f669aa8fbb6913a4a6a99bd538276ad
alex <tangyongjin@gmail.com>
2018-08-27 22:08:38 +0800

    成功加入WebRCT,build 成功

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
   
-#lsof -i :8081|grep node| awk '{print $2}'|uniq |xargs kill
+lsof -i :8081|grep node| awk '{print $2}'|uniq |xargs kill
 #ps -ef|grep 'MacOS/Terminal'|awk '{print $2}'|uniq|xargs kill  -9
 
 react-native run-android
