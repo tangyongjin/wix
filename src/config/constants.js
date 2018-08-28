6c780ea8f6c2d633baf197892d23855d0fbf5a2f
alex <tangyongjin@gmail.com>
2018-08-28 11:57:49 +0800

    设置api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,8 +3,8 @@ import ConfigStore from '../mobx/ConfigStore'
 import AnnounceStore from '../mobx/AnnounceStore'
 
 export const ACCESS_TOKEN= "access_token"
-export const API_ROOT = 'http://47.92.72.19:8010/'
+export const API_ROOT = 'http://47.92.72.19/'
 
-global.API_ROOT = 'http://47.92.72.19:8010/'
+global.API_ROOT = 'http://47.92.72.19/'
 global.ConfigStore=ConfigStore
 global.AnnounceStore=AnnounceStore
